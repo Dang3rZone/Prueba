9f8ab7b04aea16f7307e91b52a86c97c1c7231be
Andres <andresrosalespinzon@gmail.com>
2023-10-04 11:25:30 +0200

    feat: fetch the img

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import { useEffect, useState } from 'react';
 
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&jason=true`;
 export function App () {
-  const [fact, setFact] = useState('lorem cat facts');
-  // can't use axios, not allowed
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  // can't use axios/react query, not allowed
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((response) => response.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const firstThreeWords = fact.split(' ', 3).join(' ');
+        console.log(firstThreeWords);
+
+        fetch(
+          `https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            const { url } = response;
+            setImageUrl(url);
+          });
+      });
   }, []);
   return (
     <main>
-      <h1>App de mascotas</h1>)<p>{fact}</p>
+      <h1>App de mascotas</h1>
+      {fact && <p>Fact: {fact}</p>}
+      {imageUrl && (
+        <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt='image of a cat' />
+      )}
     </main>
   );
 }
